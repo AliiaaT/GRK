72caf967478251128fe4bd6da497d677b489bdb4
AliiaaT <aliya20028@gmail.com>
2022-10-10 22:53:58 +0200

    house

--- Introduction/house/script.js
+++ Introduction/house/script.js
@@ -1,4 +1,3 @@
-
 function setup() {
     createCanvas(800, 600);
     noLoop();
@@ -6,13 +5,43 @@ function setup() {
 }
 
 function draw() {
-    background(0);
-    var col = 0;
-    for (y = 0; y < height; y++) {
-        col = 0;
+    //sky
+    background(135, 206, 250);
+
+    //trava
+    var trawa = color(0, 100, 0);
+    for (y = 400; y < height; y++) {
         for (x = 0; x < width; x++) {
-            set(x, y, x / width * 256);
+            set(x, y, trawa);
         }
     }
+
+    //flowers
+    for (i = 0; i < 1000; i++) {
+        var rand_x = floor(random(0, width));
+        var rand_y = floor(random(400, height));
+        set(rand_x, rand_y, color(random(0, 255)))
+    }
+
+    //facade
+    var fasada = color(139, 69, 19);
+    for (y = 200; y < 400; y++) {
+        for (x = 200; x < width - 200; x++) {
+            set(x, y, fasada);
+        }
+    }
+
+    //roof
+    var dach = color(250, 128, 114);
+    var start_x = 125;
+    var end_x = 675;
+    for (y = 200; y > 50; y--) {
+        for (x = start_x; x < end_x; x++) {
+            set(x, y, dach);
+        }
+        start_x += 2;
+        end_x -= 2;
+    }
+
     updatePixels();
 }
