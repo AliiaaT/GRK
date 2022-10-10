4de93e49593935b3c3ba4fd023e934a33019d107
AliiaaT <aliya20028@gmail.com>
2022-10-10 22:44:30 +0200

    pixels

---
+++ Introduction/2/script.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(800, 600);
+    noLoop();
+
+}
+
+function draw() {
+    background(0);
+    for (y = 0; y < height; y++) {
+        for (x = 0; x < width; x++) {
+            set(x, y, color(100, 0, 100));
+        }
+    }
+    updatePixels();
+}
